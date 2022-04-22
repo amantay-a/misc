5fed4836305de4e5ceb441364f065852cf6e216b
Amantay <54518606+amantay-a@users.noreply.github.com>
2022-04-22 17:13:56 +1200

    first written

---
+++ uniswap_utils.py
@@ -0,0 +1,102 @@
+import logging
+from web3 import Web3
+
+abi="""[
+     {"name":"factory",
+      "inputs":[],"constant":true, "outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view",
+      "type":"function"}
+    ,{"name":"decimals",
+      "inputs":[],"outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view",
+      "type":"function"}
+    ,{"name":"getPool",
+      "inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"},{"internalType":"uint24","name":"","type":"uint24"}],"outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view",
+      "type":"function"}
+    ,{"name":"observe",
+      "inputs":[{"internalType":"uint32[]","name":"secondsAgos","type":"uint32[]"}],"outputs":[{"internalType":"int56[]","name":"tickCumulatives","type":"int56[]"},{"internalType":"uint160[]","name":"secondsPerLiquidityCumulativeX128s","type":"uint160[]"}],"stateMutability":"view",
+      "type":"function"}
+    ,{"name":"token0",
+      "inputs":[],"outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view",
+      "type":"function"}
+    ,{"name":"token1",
+      "inputs":[],"outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view",
+      "type":"function"}
+    ]
+    """
+
+UniV3Router = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+UniPoolFee = 3000 #0.3%
+
+decimals = {}
+pools = {}
+tokens0 = {}
+
+def getPoolPrice(w3,
+                 tokensA,
+                 tokensB = {'0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'}, #Default WETH
+                 TWAPWindows = [60, 300, 600, 3600], #measured in seconds
+                 blockNumber = None,
+                 Router = UniV3Router,
+                 PoolFee = UniPoolFee, # 3000 for 0.3%
+                ):
+    ret_price = {}
+    UniRouter = Web3.toChecksumAddress(Router)
+    UniFactory = w3.eth.contract(address=UniRouter, abi=abi).functions.factory().call()
+    logging.info(f'UniV3Factory: {UniFactory}')
+    if not blockNumber:
+        blockNumber = 'latest'
+    if PoolFee <=1:
+        PoolFee*=10**6
+    for t_a in tokensA:
+        t_a = Web3.toChecksumAddress(t_a)
+        if not t_a in decimals:
+            decimals[t_a] = w3.eth.contract(address=t_a, abi=abi).functions.decimals().call()
+        decimals_a = decimals[t_a]
+
+        for t_b in tokensB:
+            t_b = Web3.toChecksumAddress(t_b)
+            if not t_b in decimals:
+                decimals[t_b] = w3.eth.contract(address=t_b, abi=abi).functions.decimals().call()
+            decimals_b = decimals[t_b]
+
+            pool = [x for x in pools if pools[x] == {t_a, t_b, PoolFee}]
+            if not pool:
+                pool = w3.eth.contract(address=UniFactory, abi=abi).functions.getPool(t_a, t_b, PoolFee).call()
+                t0 = w3.eth.contract(address=pool, abi=abi).functions.token0().call()
+                pools[pool] = {t_a, t_b, PoolFee}
+                tokens0[pool] = t0
+            else:
+                pool = pool[0]
+                t0 = tokens0[pool]
+
+            if t0==t_a:
+                t1 = t_b
+                decimals0 = decimals_a
+                decimals1 = decimals_b
+            else:
+                t1=t_a
+                decimals0 = decimals_b
+                decimals1 = decimals_a
+
+            logging.info(f'pool: {pool}, token0={t0}, decimals0={decimals0}, token1={t1}, decimals1={decimals0}')
+
+            ticks = w3.eth.contract(address=pool, abi=abi).functions.observe(TWAPWindows+[0]).call(block_identifier = blockNumber)[0]
+
+            price = [1.0001**((ticks[-1] - ticks[i])/x) for i, x in enumerate(TWAPWindows)]
+            logging.debug(f'pool: {pool}, step 1, price= {price}')
+
+            price = [x*pow(10, decimals0)/pow(10, decimals1) for x in price]
+            logging.debug(f'pool: {pool}, step 2, price= {price}')
+
+            if t0!=t_a: #reverse price
+                price = [1/x for x in price]
+            logging.debug(f'pool: {pool}, step 3, price= {price}')
+
+            price = {TWAPWindows[i]:x for i, x in enumerate(price)}
+            logging.debug(f'pool: {pool}, step 4, price= {price}')
+
+            if blockNumber in ret_price:
+                ret_price[blockNumber][t_a+'-'+t_b] = {'pool':pool, 'twap': price}
+            else:
+                ret_price[blockNumber] = {t_a+'-'+t_b :{'pool':pool, 'twap': price}}
+
+    return ret_price
